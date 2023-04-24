41c7147911fc3c276bcf39249aabcd3eccaba8c5
Nurdev <nuriswanto66@gmail.com>
2023-04-24 10:34:23 -0700

    Show Image from Recycle View to Adapter

--- app/src/main/java/com/example/kotlinandroidretofitapi/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinandroidretofitapi/MainAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.kotlinandroidretofitapi
 
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MainAdapter(val results : ArrayList<MainModel.Result>)
     : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
@@ -19,10 +22,17 @@ class MainAdapter(val results : ArrayList<MainModel.Result>)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val result = results[position]
         holder.textView.text = result.title
+        Glide.with( holder.view )
+            .load(result.image)
+            .placeholder(R.drawable.img_placeholder)
+            .error(R.drawable.img_placeholder)
+            .centerCrop()
+            .into(holder.imageView)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view){
         val textView = view.findViewById<TextView>(R.id.idTextView)
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
     }
 
     fun setData(data: List<MainModel.Result>){
